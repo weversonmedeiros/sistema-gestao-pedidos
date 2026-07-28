8db7cabf900f67545eb34f961c959cfc8eb22032
WEVERSON GARCIA MEDEIROS <weverson311@gmail.com>
2026-07-28 02:34:53 -0300

    chore: setup inicial do projeto spring boot

---
+++ src/main/java/com/weverson/gestao_pedidos/GestaoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.weverson.gestao_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPedidosApplication.class, args);
+	}
+
+}
